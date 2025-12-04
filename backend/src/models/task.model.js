404ce0d03d92bf79d229f022889213f8e839ad0a
Juan David Gomez <gomezninoj681@gmail.com>
2025-12-04 13:43:23 -0500

    feat: :rocket: Se implmenta el modelo de tareas y operaciones para el CRUD con SQL

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,72 @@
+const db = require('../config/db.js')
+
+const Task = {
+    //Crear Tarea
+    async createTask(userId, title, description) {
+        const [result] = await db.query(
+            'INSERT INTO tasks (userId, title, description) VALUES (?, ?, ?)'
+            [userId, title, description]
+        )
+        return result.insertId
+    },
+
+    //Obtener tarea del Usuario
+    async getTasksByUser(userId) {
+        const [rows] = await db.query(
+            'SELECT * FROM tasks WHERE userId = ? ORDER BY createdAt DESC'
+            [userId]
+        )
+        return rows
+    },
+
+    // Obtener una tarea por id y usuario (validación)
+    async getTaskById(id, userId) {
+        const [rows] = await db.query(
+            'SELECT * FROM tasks WHERE id = ? Aand userId = ?'
+            [id, userId]
+        )
+        return rows
+    },
+
+    // Actualizar tarea
+    async updateTask(id, userId, fields) {
+        let query = 'UPDATE tasks SET'
+        const values = []
+
+        if(fields.title){
+            query += 'title = ?, '
+            values.push(fields.title)
+        }
+
+        if(fields.description !== undefined) {
+            query += 'description = ?, '
+            values.push(fields.description)
+        }
+
+        if(fields.status) {
+            query += 'status = ?'
+            values.push(fields.status)
+        } 
+
+        //Quitar la ultima coma
+        query = query.slice(0, -2)
+        query += 'WHERE id = ? AND userId = ?'
+
+        values.push(id, userId)
+
+        const [result] = await db.query(query, values)
+        return result.affectedRows > 0
+
+    }, 
+
+    //Eliminar tarea
+    async deleteTask(id, userId) {
+        cosnt [result] = await db.query(
+            'DELETE FROM tasks WHERE id = ? AND userId = ?'
+            [id, userId]
+        )
+        return result.affectedRows > 0
+    }
+}
+
+module.exports = Task
